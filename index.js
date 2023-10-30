77d618c855b5cc56cde711051224edead040da47
Tooma81 <toomas.kaarel@gmail.com>
2023-10-30 23:31:51 +0200

    Created form request response with API data

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const fetch = require("node-fetch")
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 const key = "27991efe149b6f574146c6f40d28f55d"
 let city = "Tartu"
 
@@ -17,7 +20,25 @@ app.get("/", function (req,res) {
 	.then((data) => {
 		let description = data.weather[0].description
 		let city = data.name
-		let temp = Math.round(parseFloat(data.main.temp)-273.15)
+		let temp = Math.round(parseFloat(data.main.temp)-273.15) + "°C"
+		res.render("index", {
+			description: description,
+			city: city,
+			temp: temp
+		})
+	})
+})
+
+app.post("/", function(req, res){
+	let city = req.body.cityname
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	.then((response) => {
+		return response.json()
+	})
+	.then ((data) => {
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273.15) + "°C"
 		res.render("index", {
 			description: description,
 			city: city,
